07618936999aa8950ca1dc7df532ebc802d0133b
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-04-02 20:42:29 -0300

    Assignment 1

---
+++ assignment-1/assignment.js
@@ -0,0 +1,18 @@
+// 1
+let userInput = 23;
+let result;
+
+// 2
+result = 18 + userInput;
+
+// 3
+result -= 2;
+result *= 2;
+result /= 2;
+
+// 4
+// The value in userInput did not change
+
+// 5
+alert(userInput);
+alert(result);
