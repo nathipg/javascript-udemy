befb269f1f7015b38ec24b90ef2a49fe3bcb6846
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-04-02 19:55:26 -0300

    Data types: Numbers & Strings

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,6 @@
 let currentResult = 0;
+let calculationDescription = `(${currentResult} + 10) * 3 / 2 - 1`;
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-outputResult(currentResult, '(0 + 10) * 3 / 2 - 1');
+outputResult(currentResult, calculationDescription);
