b576f2ba25c2a620ec4b15a572831ed77f36b0a6
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-04-02 23:43:02 -0300

    Functions

--- calculator/assets/scripts/app.js
+++ calculator/assets/scripts/app.js
@@ -1,7 +1,13 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+alert(add(1, 1));
+
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
