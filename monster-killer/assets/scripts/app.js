f5f06301f4e37b60f46e9a0a85d204a07fdc9ea4
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-04-10 14:54:35 -0300

    Reset game

--- monster-killer/assets/scripts/app.js
+++ monster-killer/assets/scripts/app.js
@@ -10,6 +10,12 @@ let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
+function reset() {
+  currentMonsterHealth = chosenMaxLife;
+  currentPlayerHealth = chosenMaxLife;
+  resetGame(chosenMaxLife);
+}
+
 function endRound() {
   const initialPlayerHealth = currentPlayerHealth;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
@@ -28,6 +34,10 @@ function endRound() {
     alert('You lost!');
   } else if(currentPlayerHealth < 0 && currentMonsterHealth < 0) {
     alert('You have a draw!');
+  }
+  
+  if(currentPlayerHealth < 0 || currentMonsterHealth < 0) {
+    reset();
   }
 }
 
