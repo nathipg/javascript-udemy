0019fc0cbe5017fb0e3fbdb2e5b52f6740f6b3ac
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-04-15 21:31:48 -0300

    Assignment 5

---
+++ assignment-5/assignment.js
@@ -0,0 +1,14 @@
+// 1
+const firstTask = document.getElementById('task-1');
+const firstTask2 = document.querySelector('#task-1');
+
+firstTask.style.backgroundColor = 'black';
+firstTask2.style.color = 'white';
+
+// 2
+const title = document.head.querySelector('title');
+title.textContent = 'Assignment - Solved!'
+
+// 3
+const h1 = document.querySelector('h1');
+h1.textContent = 'Assignment - Solved!';
