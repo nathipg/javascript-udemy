aa626e40465f9905c698ef05ba0e161bff0f4974
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-04-17 11:39:25 -0300

    indexOf() & lastIndexOf()

--- arrays/app.js
+++ arrays/app.js
@@ -72,10 +72,13 @@ console.log([...someTestResults]);
 const someMoreTestResults = testResults.slice(3);
 console.log([...someMoreTestResults]); */
 
-const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
 const storedResults = testResults.concat([3.99, 2]);
 
 testResults.push(5.91);
 
 console.log([...testResults]);
 console.log([...storedResults]);
+
+console.log(testResults.indexOf(1.5));
+console.log(testResults.lastIndexOf(1.5));
