46e13d272f107bbeca01ef306b4310f7960c13b4
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-04-17 11:19:20 -0300

    Creatings arrays

---
+++ arrays/app.js
@@ -0,0 +1,31 @@
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+/* const moreNumbers = new Array(1, 2, 3); // Also works without new keyword
+const moreNumbers2 = new Array(5); // Create empty array with length 5
+console.log(moreNumbers);
+console.log(moreNumbers2);
+
+const yetMoreNumbers = Array.of(1, 2);
+console.log(yetMoreNumbers); */
+
+const listItems = document.querySelectorAll('li');
+console.log(listItems);
+
+const arrayListItems = Array.from(listItems);
+console.log(arrayListItems);
+
+const textAsArray = Array.from('Hi!');
+console.log(textAsArray);
+
+const hobbies = ['Cooking', 'Sports'];
+const personalData = [30, 'Max', {modeDetail: []}];
+const analyticsData = [[1, 1.6], [-5.4, 2.1]];
+
+for(const data of analyticsData) {
+  for(const dataPoint of data) {
+    console.log(data);
+  }
+}
+
+console.log(personalData[1]);
