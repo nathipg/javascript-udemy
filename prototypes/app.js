22711c0574ddbe4790495689dbb3fcf4949324df
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-04-23 18:44:52 -0300

    Classes & Prototypes

--- prototypes/app.js
+++ prototypes/app.js
@@ -1,29 +1,29 @@
-// class AgedPerson {
-//   printAge() {
-//     console.log(this.age);
-//   }
-// }
-
-// class Person extends AgedPerson {
-//   name = 'Pissuti';
-
-//   constructor() {
-//     super();
-//     this.age = 23;
-//   }
-
-//   greet() {
-//     console.log(`Name: ${this.name} Age: ${this.age}`);
-//   }
-// }
-
-function Person() {
-  this.age = 23;
-  this.name = 'Pissuti';
-  this.greet = function () {
-    console.log(`Name: ${this.name} Age: ${this.age}`);
-  };
+class AgedPerson {
+  printAge() {
+    console.log(this.age);
+  }
 }
+
+class Person extends AgedPerson {
+  name = 'Pissuti';
+
+  constructor() {
+    super();
+    this.age = 23;
+  }
+
+  greet() {
+    console.log(`Name: ${this.name} Age: ${this.age}`);
+  }
+}
+
+// function Person() {
+//   this.age = 23;
+//   this.name = 'Pissuti';
+//   this.greet = function () {
+//     console.log(`Name: ${this.name} Age: ${this.age}`);
+//   };
+// }
 
 // Person.prototype = {
 //   printAge() {
@@ -31,20 +31,24 @@ function Person() {
 //   }
 // };
 
-Person.describe = function() { // Same as define static method
-  console.log('Creating persons...');
-}
+// Person.describe = function() { // Same as define static method
+//   console.log('Creating persons...');
+// }
 
-Person.prototype.printAge = function () { // Same as extends class with this method
-  console.log(this.age);
-};
+// Person.prototype.printAge = function () { // Same as extends class with this method
+//   console.log(this.age);
+// };
 
+// console.log(Person);
+// Person.describe();
+
+// const person = new Person();
+// person.greet();
+// person.printAge();
+
+// console.log(person);
+// console.dir(Object.prototype);
+
+const p = new Person();
 console.log(Person);
-Person.describe();
-
-const person = new Person();
-person.greet();
-person.printAge();
-
-console.log(person);
-console.dir(Object.prototype);
+console.log(p);
