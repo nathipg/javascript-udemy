05e5b4fb7e051ba69eb1a3cf5d332681aba8d6e4
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-05-19 17:25:18 -0300

    How Numbers Work & Behave in Javascript

---
+++ nums-strings/app.js
@@ -0,0 +1,3 @@
+console.log('MAX SAFE INTEGER', Number.MAX_SAFE_INTEGER); // Same as 2 ^ 53 - 1
+console.log('MIN SAFE INTEGER', Number.MIN_SAFE_INTEGER);
+console.log('MAX VALUE', Number.MAX_VALUE);
