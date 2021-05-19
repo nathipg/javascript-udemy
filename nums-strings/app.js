2f7cff0982b4f7ce4823741c3604f73fafd0a968
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-05-19 17:37:06 -0300

    Floating Point (Im)Precision

--- nums-strings/app.js
+++ nums-strings/app.js
@@ -1,3 +1,14 @@
 console.log('MAX SAFE INTEGER', Number.MAX_SAFE_INTEGER); // Same as 2 ^ 53 - 1
 console.log('MIN SAFE INTEGER', Number.MIN_SAFE_INTEGER);
 console.log('MAX VALUE', Number.MAX_VALUE);
+
+
+console.log(`0.2 + 0.4 = ${0.2 + 0.4}`);
+console.log(`0.2 + 0.4 === 0.6? ${0.2 + 0.4 === 0.6}`);
+
+console.log((5).toString(2)); // 5 in binary system
+console.log(0.2.toString(2));
+console.log(0.2.toFixed(20));
+console.log((0.2 + 0.4).toFixed(2));
+
+console.log(20.2.toFixed(20));
