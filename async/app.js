25891267798217642a5ccfb06cc65f21862aa0fa
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-06-11 17:39:47 -0300

    Sync + Async code - Execution Order

--- async/app.js
+++ async/app.js
@@ -2,7 +2,21 @@ const button = document.querySelector('button');
 const output = document.querySelector('p');
 
 function trackUserHandler() {
-  console.log('Clicked!');
+  navigator.geolocation.getCurrentPosition(posData => {
+    console.log(posData);
+  }, error => {
+    console.log(error)
+  });
+
+  console.log('Getting position...');
 }
 
 button.addEventListener('click', trackUserHandler);
+
+// let result = 0;
+
+// for(let i = 0; i < 99999999; i++) {
+//   result++;
+// }
+
+// console.log(result);
