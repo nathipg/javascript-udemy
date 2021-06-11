41bcacab4c9c7c4635c98e5be73e67ed08de5c9d
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-06-11 18:03:31 -0300

    Chaining multiple promises

--- async/app.js
+++ async/app.js
@@ -1,6 +1,22 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const getPosition = opts => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      success => {
+        resolve(success);
+      },
+      error => {
+        reject(error);
+      },
+      opts
+    );
+  });
+
+  return promise;
+};
+
 const setTimer = duration => {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -12,14 +28,15 @@ const setTimer = duration => {
 };
 
 function trackUserHandler() {
-  navigator.geolocation.getCurrentPosition(posData => {
-    setTimer(2000)
-      .then(data => {
-        console.log(data, posData);
-      });
-  }, error => {
-    console.log(error)
-  });
+  let positionData = null;
+  getPosition()
+    .then(posData => {
+      positionData = posData;
+      return setTimer(2000);
+    })
+    .then(data => {
+      console.log(data, positionData);
+    });
 
   setTimer(0)
     .then(() => {
