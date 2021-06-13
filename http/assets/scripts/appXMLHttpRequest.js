68d785216233637eeca145402de41fb8212da62b
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-06-13 17:48:14 -0300

    Using the fetch() API

--- http/assets/scripts/appXMLHttpRequest.js
+++ http/assets/scripts/appXMLHttpRequest.js
@@ -5,9 +5,29 @@ const fetchButton = document.querySelector('#available-posts button');
 const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url, data) {
-  return fetch(url).then(response => {
-    return response.json();
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(method, url);
+
+    xhr.responseType = 'json';
+
+    xhr.onload = function() {
+      if(xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject(new Error('Something went wrong'));
+      }
+    };
+
+    xhr.onerror = function() {
+      reject(new Error('Failed to send request'));
+    }
+
+    xhr.send(JSON.stringify(data));
   });
+
+  return promise;
 }
 
 async function fetchPosts() {
