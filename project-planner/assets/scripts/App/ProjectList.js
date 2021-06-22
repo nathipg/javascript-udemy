61bdadc0587edbb5b1fb464ab98275e3016e129c
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-06-22 20:33:41 -0300

    named export syntax variations

--- project-planner/assets/scripts/App/ProjectList.js
+++ project-planner/assets/scripts/App/ProjectList.js
@@ -1,5 +1,5 @@
 import ProjectItem from './ProjectItem.js';
-import DOMHelper from '../Utility/DOMHelper.js';
+import DOMHelper, * as DOMH from '../Utility/DOMHelper.js';
 
 export default class ProjectList {
   projects = [];
@@ -63,7 +63,7 @@ export default class ProjectList {
 
   addProject(project) {
     this.projects.push(project);
-    DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
+    DOMH.moveElement(project.id, `#${this.type}-projects ul`);
     project.update(this.switchProject.bind(this), this.type);
   }
 
--- project-planner/assets/scripts/Utility/DOMHelper.js
+++ project-planner/assets/scripts/Utility/DOMHelper.js
@@ -14,3 +14,18 @@ export default class DOMHelper {
     });
   }
 }
+
+export function clearEventListeners(element) {
+  const clonedElement = element.cloneNode(true);
+  element.replaceWith(clonedElement);
+  return clonedElement;
+}
+
+export function moveElement(elementId, newDestinationSelector) {
+  const element = document.getElementById(elementId);
+  const destinationElement = document.querySelector(newDestinationSelector);
+  destinationElement.append(element);
+  element.scrollIntoView({
+    behavior: 'smooth',
+  });
+}
