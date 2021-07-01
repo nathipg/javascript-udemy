a2575baa9e49f563cf2135aac333cf3e453b17e5
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-06-30 21:21:15 -0300

    final optimization

--- project-planner/webpack.config.prod.js
+++ project-planner/webpack.config.prod.js
@@ -1,16 +1,17 @@
 /* eslint-disable no-undef */
 const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'production',
   entry: './src/app.js',
   output: {
-    filename: 'app.js',
+    filename: '[contenthash].js',
     path: path.resolve(__dirname, 'assets', 'scripts'),
     publicPath: 'assets/scripts/'
   },
-  devtool: 'cheap-source-map'
-  // devServer: {
-  //   contentBase: './'
-  // }
+  devtool: 'cheap-source-map',
+  plugins: [
+    new CleanPlugin.CleanWebpackPlugin()
+  ]
 };
