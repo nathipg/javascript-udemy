9b466ccdd97ed9df4c97db7882c368c853347880
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-07-04 14:58:28 -0300

    Getting User Location

--- practice-places/src/SharePlace.js
+++ practice-places/src/SharePlace.js
@@ -7,7 +7,28 @@ class PlaceFinder {
     addressForm.addEventListener('submit', this.findAddressHandler);
   }
 
-  locateUserHandler() {}
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert('Location feature not available in your browser - please use a mora modern browser or enter an address manually!');
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      successResult => {
+        const coordinates = {
+          lat: successResult.coords.latitude,
+          lng: successResult.coords.longitude,
+        };
+
+        console.log(coordinates);
+      },
+      error => {
+        alert('Could not locate you. Please enter an address manually!');
+      }
+    );
+  }
 
   findAddressHandler() {}
 }
+
+new PlaceFinder();
