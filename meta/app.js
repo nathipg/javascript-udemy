729c1a02b57b6381ee4c0c940298e16988bb0974
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-07-07 21:45:40 -0300

    Generators & Iterable Objects

--- meta/app.js
+++ meta/app.js
@@ -20,27 +20,69 @@ console.log(Symbol('uid') === Symbol('uid')); // false
 
 console.log(user.toString());
 
-// Iterators
+// Iterators & Generators
 const company = {
   curEmployee: 0,
   employees: ['Pissuti', 'Apollo', 'Joana'],
-  next() {
-    let done = false;
-    if (this.curEmployee >= this.employees.length) {
-      done = true;
-    }
-    const returnValue = {
-      value: this.employees[this.curEmployee],
-      done: done,
-    };
-    this.curEmployee++;
+  // next() {
+  //   let done = false;
+  //   if (this.curEmployee >= this.employees.length) {
+  //     done = true;
+  //   }
+  //   const returnValue = {
+  //     value: this.employees[this.curEmployee],
+  //     done: done,
+  //   };
+  //   this.curEmployee++;
 
-    return returnValue;
+  //   return returnValue;
+  // },
+  getEmployee: function* employeeGenerator() {
+    // let employee = company.next();
+
+    // while(!employee.done) {
+    //   yield employee.value;
+    //   employee = company.next();
+    // }
+
+    let currentEmployee = 0;
+    while(currentEmployee < this.employees.length) {
+      yield this.employees[currentEmployee];
+      currentEmployee++;
+    }
+  },
+  [Symbol.iterator]: function* employeeGenerator() {
+    // let employee = company.next();
+
+    // while(!employee.done) {
+    //   yield employee.value;
+    //   employee = company.next();
+    // }
+
+    let currentEmployee = 0;
+    while(currentEmployee < this.employees.length) {
+      yield this.employees[currentEmployee];
+      currentEmployee++;
+    }
   },
 };
 
-let employee = company.next();
-while(!employee.done) {
-  console.log(employee.value);
-  employee = company.next();
+// let employee = company.next();
+// while(!employee.done) {
+//   console.log(employee.value);
+//   employee = company.next();
+// }
+
+const it = company.getEmployee();
+
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+
+for(const employee of company) {
+  console.log(employee);
 }
+
+console.log([...company]);
