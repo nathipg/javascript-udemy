ac2f48e03fee19e3a9f7557809ba31a6b45df721
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-07-19 20:47:34 -0300

    Server-side HTML with templates & EJS

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
@@ -13,13 +16,9 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   const userName = req.body.username || 'Unknown User';
 
-  res.send(`
-    <form method="POST" action="/">
-      <input type="text" name="username" />
-      <button type="submit">Send</button>
-    </form>
-    <h1>Hi, ${userName}</h1>
-  `);
+  res.render('index', {
+    user: userName
+  });
 });
 
 app.listen(3000);
