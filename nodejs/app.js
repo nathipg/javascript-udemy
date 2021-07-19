6229a10597d1c16d0d5a8518ed314cbb8a9c97a0
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-07-19 20:24:13 -0300

    Parsing incoming data

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,9 +1,32 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-  response.setHeader('Content-Type', 'text/html');
-  response.write('<h1>Hello there!</h1>');
-  response.end();
+  let body = [];
+  const method = request.method;
+  const url = request.url;
+
+  request.on('data', chunk => {
+    body.push(chunk);
+  });
+  
+  request.on('end', () => {
+    body = Buffer.concat(body).toString();
+    
+    let userName = 'Unknown User';
+    if(body) {
+      userName = body.split('=')[1];
+    }
+
+    response.setHeader('Content-Type', 'text/html');
+    response.write(`
+      <form method="POST" action="/">
+        <input type="text" name="username" />
+        <button type="submit">Send</button>
+      </form>
+      <h1>Hi, ${userName}</h1>
+    `);
+    response.end();
+  });
 });
 
 server.listen(3000);
