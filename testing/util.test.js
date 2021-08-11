9fad0847265f7184db54ac1d05ebfb2028cd987c
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-08-11 20:40:23 -0300

    e2e tests

--- testing/util.test.js
+++ testing/util.test.js
@@ -1,3 +1,4 @@
+const puppetter = require('puppeteer');
 const { generateText, checkAndGenerate } = require('./util');
 
 test('should output name and age', () => {
@@ -8,4 +9,28 @@ test('should output name and age', () => {
 test('should generate a valid text output', () => {
   const text = checkAndGenerate('Pissuti', 24);
   expect(text).toBe('Pissuti (24 years old)');
-})
+});
+
+test('should create an element with text and correct class', async () => {
+  const browser = await puppetter.launch({
+    headless: true,
+    // slowMo: 80,
+    // args: ['--window-size=1920,1080'],
+  });
+
+  const page = await browser.newPage();
+  await page.goto(
+    'file:///C:/Users/nathipg/github/javascript-udemy/testing/index.html'
+  );
+
+  await page.click('input#name');
+  await page.type('input#name', 'Pissuti');
+
+  await page.click('input#age');
+  await page.type('input#age', '24');
+
+  await page.click('#btnAddUser');
+
+  const finalText = await page.$eval('.user-item', el => el.textContent);
+  expect(finalText).toBe('Pissuti (24 years old)');
+}, 10000);
