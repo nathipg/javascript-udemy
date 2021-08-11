144e7348a58285254b71dac3d9e5ba173163a4b9
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-08-11 20:14:25 -0300

    Integration tests

--- testing/util.test.js
+++ testing/util.test.js
@@ -1,6 +1,11 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
 test('should output name and age', () => {
   const text = generateText('Pissuti', 24);
   expect(text).toBe('Pissuti (24 years old)');
 });
+
+test('should generate a valid text output', () => {
+  const text = checkAndGenerate('Pissuti', 24);
+  expect(text).toBe('Pissuti (24 years old)');
+})
