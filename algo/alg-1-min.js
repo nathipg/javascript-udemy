8883cf70d45f5377990d2c1098a0946d14b9f8e3
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-08-12 21:12:29 -0300

    Different solution

--- algo/alg-1-min.js
+++ algo/alg-1-min.js
@@ -14,8 +14,33 @@ function getMin(numbers) {
   return currentMin;
 }
 
-const testNumbers = [3, 1, 2];
+function getMin2(numbers) {
+  if (!numbers.length) {
+    throw new Error('Empty array');
+  }
+
+  for (let i = 0; i < numbers.length; i++) {
+    let outerElement = numbers[i];
+    for (let j = i + 1; j < numbers.length; j++) {
+      let innerElement = numbers[j];
+
+      if (outerElement > innerElement) {
+        numbers[i] = innerElement;
+        numbers[j] = outerElement;
+
+        outerElement = numbers[i];
+        innerElement = numbers[j];
+      }
+    }
+  }
+
+  return numbers[0];
+}
+
+const testNumbers = [2, 1, -5, 10, 10, -10];
 
 const min = getMin(testNumbers);
+const min2 = getMin2(testNumbers);
 
 console.log(min);
+console.log(min2);
