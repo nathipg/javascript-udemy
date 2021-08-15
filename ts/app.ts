62a56a05295ccf4efce554865963460a3e5db45e
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-08-15 19:27:14 -0300

    Object Types, Array Types and Functions Types

--- ts/app.ts
+++ ts/app.ts
@@ -1,5 +1,5 @@
 const num1Input = document.getElementById('num1') as HTMLInputElement;
-const num2Input = <HTMLInputElement> document.getElementById('num2');
+const num2Input = <HTMLInputElement>document.getElementById('num2');
 const buttonElement = document.querySelector('button') as HTMLButtonElement;
 
 function add(a: number, b: number) {
@@ -15,9 +15,19 @@ const isDone = false;
 
 printResult(result);
 
+let results: { res: number; print: () => void }[] = [];
+const names = ['Pissuti'];
+
 buttonElement.addEventListener('click', () => {
   const num1 = +num1Input.value;
   const num2 = +num2Input.value;
   const result = add(num1, num2);
-  printResult(result);
+  const resultContainer = {
+    res: result,
+    print() {
+      console.log(this.res);
+    },
+  };
+  results.push(resultContainer);
+  results[0].print();
 });
