d8c7a53a6e028cdfcc1af05b78920385542e0b74
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-08-18 18:46:36 -0300

    connectedCallback

--- web-components/tooltip.js
+++ web-components/tooltip.js
@@ -1,12 +1,13 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super();
+  }
+
+  connectedCallback() {
     const tooltipIcon = document.createElement('span');
     tooltipIcon.textContent = ' (?)';
     this.appendChild(tooltipIcon);
   }
-
-  connectedCallback() {}
 
   disconnectedCallback() {}
 
