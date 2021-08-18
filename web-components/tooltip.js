e7bf1ae8b649d4bf89940798768c017e907ed357
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-08-18 19:04:28 -0300

    Styling our elements

--- web-components/tooltip.js
+++ web-components/tooltip.js
@@ -17,6 +17,7 @@ class Tooltip extends HTMLElement {
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
 
     this.appendChild(tooltipIcon);
+    this.style.position = 'relative';
   }
 
   disconnectedCallback() {}
@@ -26,6 +27,10 @@ class Tooltip extends HTMLElement {
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
     this._tooltipContainer.textContent = this._tooltipText;
+    this._tooltipContainer.style.backgroundColor = 'black';
+    this._tooltipContainer.style.color = 'white';
+    this._tooltipContainer.style.position = 'absolute';
+    this._tooltipContainer.style.zIndex = 10;
     this.appendChild(this._tooltipContainer);
   }
 
