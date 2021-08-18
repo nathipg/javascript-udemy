9b568d0f218c8c4229cd6aa24bd168832f0fbca8
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-08-18 19:16:00 -0300

    HTML Template

--- web-components/tooltip.js
+++ web-components/tooltip.js
@@ -6,6 +6,8 @@ class Tooltip extends HTMLElement {
     this.attachShadow({
       mode: 'open',
     });
+    const template = document.querySelector('#tooltip-template');
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 
   connectedCallback() {
@@ -13,8 +15,7 @@ class Tooltip extends HTMLElement {
       this._tooltipText = this.getAttribute('text');
     }
 
-    const tooltipIcon = document.createElement('span');
-    tooltipIcon.textContent = ' (?)';
+    const tooltipIcon = this.shadowRoot.querySelector('span');
 
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
